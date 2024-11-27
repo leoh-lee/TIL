6f908f9f897819b19b2b1837bb5ebef48150ac8a
nimoh <spakers38@gmail.com>
2024-11-27 23:12:33 +0900

    [vue-prac] props 연습

---
+++ vue_practice/src/components/ChildComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ props.sendProps1 }}</div>
+  <div>{{ props.sendProps2 }}</div>
+  <div>{{ props.sendProps3.name }}</div>
+</template>
+
+<script setup>
+const props = defineProps({
+  sendProps1: String,
+  sendProps2: Number,
+  sendProps3: Object
+})
+
+console.log(props)
+</script>
